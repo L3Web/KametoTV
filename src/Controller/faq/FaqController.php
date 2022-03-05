90f7997fff46abd903da3373a483f2becffdecd4
Lin Alix <linalix19@gmail.com>
2022-03-05 16:31:19 +0100

    feat : form to add faq

--- src/Controller/faq/FaqController.php
+++ src/Controller/faq/FaqController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller\faq;
 
+use App\Entity\Faq;
+use App\Form\AddFaqFormType;
 use App\Repository\FaqRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,6 +26,26 @@ class FaqController extends AbstractController
         return $this->render('faq/faq.html.twig', [
             "faqAll"=>$res,
             "quantity"=>count($res)-1
+        ]);
+    }
+
+    #[Route('/faq/add', name: 'AddFaq')]
+    public function addFaq(Request $request,EntityManagerInterface $entityManager) : Response
+    {
+        $faq = new Faq();
+        $form = $this->createForm(AddFaqFormType::class, $faq);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($faq);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_faq');;
+        }
+
+        return $this->renderForm('faq/faqAdd.html.twig',[
+            'formFaq'=>$form
         ]);
     }
 }
