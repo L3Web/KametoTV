dc7ac868966ed911b392fefa0938aa4a5062544a
Lin Alix <linalix19@gmail.com>
2022-03-05 18:30:39 +0100

    feat : stream page

---
+++ src/Controller/stream/StreamController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller\stream;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StreamController extends AbstractController
+{
+    #[Route('/stream', name: 'stream')]
+    public function showStream()
+    {
+       return $this->render('/stream/stream.html.twig');
+    }
+
+}
