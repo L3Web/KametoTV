bae06dc20beda17ee8dcab59a928b0fccca9f175
ElodieWan <juicebouice@gmail.com>
2022-03-13 01:46:56 +0100

    fix : routing et ajout de roles

--- src/Controller/roleManagement/roleDetailController.php
+++ src/Controller/roleManagement/roleDetailController.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\NonUniqueResultException;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class roleDetailController extends AbstractController
 {
@@ -23,28 +24,20 @@ class roleDetailController extends AbstractController
         if (is_null($user)) {
             throw $this->createNotFoundException("User don't exist");
         }
-
         $hierarchy = $this->getParameter('security.role_hierarchy.roles');
         unset($hierarchy["ROLE_SUPER_ADMIN"]);
 
         $userRoles = $user->getRoles();
-
-
         //A modifier
         foreach ($userRoles as $value) {
-            foreach ($hierarchy as $key => $valueH) {
-                if (in_array($value, $valueH) !== false) {
-                    unset($hierarchy[$key]);
-                }
+            if (in_array($value, $hierarchy)) {
+                unset($hierarchy[$value]);
             }
-            unset($hierarchy[$value]);
         }
-
         $user->setRoles(array_values($userRoles));
         return $this->render('roleManagement/roleManagementDetail.html.twig', [
             "user" => $user,
             "hierarchyList" => array_keys($hierarchy)
-
         ]);
     }
 }
