a8baa8d3690370751aa0df98d063273e945ca4f0
ElodieWan <juicebouice@gmail.com>
2022-04-01 18:08:15 +0200

    optimize product controller

--- src/Controller/boutique/ProductController.php
+++ src/Controller/boutique/ProductController.php
@@ -2,17 +2,16 @@
 
 namespace App\Controller\boutique;
 
+use App\Controller\Controller;
 use App\Entity\Product;
 use App\Form\AddProductFormType;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use App\Handler\ProductFormHandler;
+use App\Services\FileUploader;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
-class ProductController extends AbstractController
+class ProductController extends Controller
 {
 
     /**
@@ -20,52 +19,26 @@ class ProductController extends AbstractController
      *
      */
 
-    public function addProd(Request $request, SluggerInterface $slugger, EntityManagerInterface $entityManager): Response
+    public function addProd(Request $request, FileUploader $fileUploader): Response
     {
         $product = new Product();
         $form = $this->createForm(AddProductFormType::class, $product);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-
-            $imageFile = $form->get('image')->getData();
-
-            if ($imageFile) {
-                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-                // this is needed to safely include the file name as part of the URL
-                $safeFilename = $slugger->slug($originalFilename);
-                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
-
-                // Move the file to the directory where brochures are stored
-                try {
-                    $imageFile->move(
-                        $this->getParameter('brochures_directory'),
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                    // ... handle exception if something happens during file upload
-                }
-
-                // updates the 'brochureFilename' property to store the PDF file name
-                // instead of its contents
-                $product->setImage($newFilename);
-
-            }
-            $entityManager->persist($product);
-            $entityManager->flush();
+        $formHandler = (new ProductFormHandler($form, $request, $product, $this->entityManager, $fileUploader))->process();
+        if ($formHandler) {
             return $this->redirectToRoute('app_boutique');
         }
-
         return $this->renderForm('boutique/addProduct.html.twig', [
             'formProduct' => $form
-            ]);
+        ]);
     }
 
-    #[Route('/boutique/{id}', name: 'app_prodDel')]
-    public function deleteFaq(Product $pro, EntityManagerInterface $entityManager): Response
+    /**
+     * @Route("/boutique/{id<^[1-9]{1}[0-9]*$>}", name="app_prodDel")
+     */
+    public function deleteProd(Product $pro): Response
     {
-        $entityManager->remove($pro);
-        $entityManager->flush();
+        $this->entityManager->remove($pro);
+        $this->entityManager->flush();
 
         return $this->redirectToRoute('app_boutique');
     }
---
+++ src/Handler/ProductFormHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Handler;
+
+use App\Entity\Product;
+use App\Services\FileUploader;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductFormHandler
+{
+    private Form $form;
+    private Request $request;
+    private Product $product;
+    private EntityManagerInterface $entityManager;
+    private FileUploader $fileUploader;
+
+    public function __construct(Form $form, Request $request, Product $product, EntityManagerInterface $entityManager, FileUploader $fileUploader)
+    {
+        $this->form = $form;
+        $this->request = $request;
+        $this->product = $product;
+        $this->entityManager = $entityManager;
+        $this->fileUploader = $fileUploader;
+    }
+
+    public function process(): bool
+    {
+        $this->form->handleRequest($this->request);
+
+        if ($this->form->isSubmitted() && $this->form->isValid()) {
+
+            $imageFile = $this->form->get('image')->getData();
+
+            if ($imageFile) {
+                $newFilename = $this->fileUploader->upload($imageFile);
+                $this->product->setImage($newFilename);
+            }
+            $this->entityManager->persist($this->product);
+            $this->entityManager->flush();
+
+            return true;
+        }
+
+        return false;
+    }
+}
