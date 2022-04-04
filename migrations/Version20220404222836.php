6f8c72a83c188d3a14ecb4d5f5c68d83e6fa9ea5
ElodieWan <juicebouice@gmail.com>
2022-04-05 00:41:08 +0200

    feat : fixture product + fix css

---
+++ migrations/Version20220404222836.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220404222836 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, category_name VARCHAR(20) NOT NULL, category_desc LONGTEXT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE commande (id INT AUTO_INCREMENT NOT NULL, id_user_id INT NOT NULL, date DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', articles LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', total DOUBLE PRECISION NOT NULL, status SMALLINT NOT NULL, INDEX IDX_6EEAA67D79F37AE5 (id_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE faq (id INT AUTO_INCREMENT NOT NULL, question VARCHAR(1000) NOT NULL, answer VARCHAR(1000) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE product (id INT AUTO_INCREMENT NOT NULL, product_name VARCHAR(255) NOT NULL, price INT NOT NULL, product_qtt INT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', modified_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', category_id INT NOT NULL, image VARCHAR(255) NOT NULL, product_desc VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE reset_password_request (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, selector VARCHAR(20) NOT NULL, hashed_token VARCHAR(100) NOT NULL, requested_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', expires_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_7CE748AA76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, password VARCHAR(200) NOT NULL, first_name VARCHAR(255) NOT NULL, last_name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, roles JSON NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', deleted_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', modified_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE commande ADD CONSTRAINT FK_6EEAA67D79F37AE5 FOREIGN KEY (id_user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE reset_password_request ADD CONSTRAINT FK_7CE748AA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE commande DROP FOREIGN KEY FK_6EEAA67D79F37AE5');
+        $this->addSql('ALTER TABLE reset_password_request DROP FOREIGN KEY FK_7CE748AA76ED395');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE commande');
+        $this->addSql('DROP TABLE faq');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('DROP TABLE reset_password_request');
+        $this->addSql('DROP TABLE user');
+    }
+}
--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -2,7 +2,75 @@
 
 namespace App\DataFixtures;
 
-class ProductFixtures
-{
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
 
+class ProductFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $products = [
+            1 => [
+                'product_name' => 'Tote bag karmine',
+                'price' => '100',
+                'product_qtt' => '20',
+                'created_at' => new \DateTimeImmutable("now"),
+                'category_id' => 1,
+                'image' => 'tote-bag-karmine-6233395f91e8e-6234e5694fa13-62444cf744968-62445db53a966-6244602e2fc78-624465a6e836c-6244663ad4126.jpg',
+                'product_desc' => 'Un tote bag noir',
+            ],
+            2 => [
+                'product_name' => 'Ultra flag',
+                'price' => '80',
+                'product_qtt' => '15',
+                'created_at' => new \DateTimeImmutable("now"),
+                'category_id' => 2,
+                'image' => 'ultraflag-624461afccee8.jpg',
+                'product_desc' => 'Quelque chose',
+            ],
+            3 => [
+                'product_name' => 'T-shirt bleu',
+                'price' => '40',
+                'product_qtt' => '30',
+                'created_at' => new \DateTimeImmutable("now"),
+                'category_id' => 1,
+                'image' => 'teebleu-624464ebd4d45.jpg',
+                'product_desc' => 'Un t-shirt bleu',
+            ],
+            4 => [
+                'product_name' => 'Casquette Kcorp',
+                'price' => '15',
+                'product_qtt' => '20',
+                'created_at' => new \DateTimeImmutable("now"),
+                'category_id' => 2,
+                'image' => 'casquetteKcorp-6244643f3f43e.jpg',
+                'product_desc' => 'Une casquette des Kcorp',
+            ],
+            5 => [
+                'product_name' => 'Gourde',
+                'price' => '100',
+                'product_qtt' => '20',
+                'created_at' => new \DateTimeImmutable("now"),
+                'category_id' => 2,
+                'image' => 'gourde-624b54c8ab3bd.jpg',
+                'product_desc' => 'Une gourde',
+            ],
+        ];
+
+        foreach ($products as $value) {
+            $products = new Product();
+            $products->setProductName($value["product_name"]);
+            $products->setPrice($value["price"]);
+            $products->setProductQtt($value["product_qtt"]);
+            $products->setCreatedAt($value["created_at"]);
+            $products->setCategoryId($value["category_id"]);
+            $products->setImage($value["image"]);
+            $products->setProductDesc($value["product_desc"]);
+
+            $manager->persist($products);
+        }
+
+        $manager->flush();
+    }
 }
